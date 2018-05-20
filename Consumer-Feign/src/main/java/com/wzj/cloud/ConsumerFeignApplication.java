4825ee711fc5a09dd321c92f5ac317e3cadaa4f0
HelloKittyNII <1291924229@qq.com>
2018-05-20 18:30:15 +0800

    SpringCloud服务调用Feign方式

---
+++ Consumer-Feign/src/main/java/com/wzj/cloud/ConsumerFeignApplication.java
@@ -0,0 +1,17 @@
+package com.wzj.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumerFeignApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerFeignApplication.class, args);
+	}
+}
