dfeb2d15efa420c5b006eef6c915f6ac453942ee
HelloKittyNII <1291924229@qq.com>
2018-05-28 21:53:38 +0800

    上传zuul路由

---
+++ SpringCloudFramework/Service-Zuul/src/main/java/com/wzj/cloud/MyFilter.java
@@ -0,0 +1,88 @@
+package com.wzj.cloud;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by wzj on 2018/5/24.
+ */
+@Component
+public class MyFilter extends ZuulFilter
+{
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    /**
+     * to classify a filter by type. Standard types in Zuul are "pre" for pre-routing filtering,
+     * "route" for routing to an origin, "post" for post-routing filters, "error" for error handling.
+     * We also support a "static" type for static responses see  StaticResponseFilter.
+     * Any filterType made be created or added and run by calling FilterProcessor.runFilters(type)
+     *
+     * @return A String representing that type
+     */
+    @Override
+    public String filterType()
+    {
+        return "pre";
+    }
+
+    /**
+     * filterOrder() must also be defined for a filter. Filters may have the same  filterOrder if precedence is not
+     * important for a filter. filterOrders do not need to be sequential.
+     *
+     * @return the int order of a filter
+     */
+    @Override
+    public int filterOrder()
+    {
+        return 0;
+    }
+
+    /**
+     * a "true" return from this method means that the run() method should be invoked
+     *
+     * @return true if the run() method should be invoked. false will not invoke the run() method
+     */
+    @Override
+    public boolean shouldFilter()
+    {
+        return true;
+    }
+
+    /**
+     * if shouldFilter() is true, this method will be invoked. this method is the core method of a ZuulFilter
+     *
+     * @return Some arbitrary artifact may be returned. Current implementation ignores it.
+     * @throws ZuulException if an error occurs during execution.
+     */
+    @Override
+    public Object run() throws ZuulException
+    {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null)
+        {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try
+            {
+                ctx.getResponse().getWriter().write("token is empty");
+            }
+            catch (Exception e)
+            {
+            }
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
